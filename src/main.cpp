291c88bdb1d7e55ebb1e8d9f61ca1199059111a4
alexandermeek <alexandermeek10@outlook.com>
2020-05-27 21:09:26 +0100

    Add simple 3f sphere intersection.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 #include <immintrin.h>
 
 #define f8 __m256
@@ -10,17 +11,49 @@
 #define cmp8(a, b, i) _mm256_cmp_ps(a, b, i)
 #define mm8(a) _mm256_movemask_ps(a)
 #define bld8(a, b, m) _mm256_blendv_ps(a, b, m)
+#define sqrt8(a) _mm256_sqrt_ps (a)
 
 struct vec3f { 
     float x, y, z;
 
-    vec3f operator+(const vec3f &rhs) {
+    vec3f() : x(0), y(0), z(0) {}
+    vec3f(float x, float y, float z) : x(x), y(y), z(z) {}
+
+    vec3f operator+(const vec3f &rhs) const {
         vec3f res;
         res.x = x + rhs.x;
         res.y = y + rhs.y;
         res.z = z + rhs.z;
         return res;
-    }    
+    }
+
+    vec3f operator-(const vec3f &rhs) const {
+        vec3f res;
+        res.x = x - rhs.x;
+        res.y = y - rhs.y;
+        res.z = z - rhs.z;
+        return res;
+    }
+
+    vec3f operator*(const vec3f &rhs) const {
+        vec3f res;
+        res.x = x * rhs.x;
+        res.y = y * rhs.y;
+        res.z = z * rhs.z;
+        return res;
+    }
+
+    vec3f operator/(const vec3f &rhs) const {
+        vec3f res;
+        res.x = x / rhs.x;
+        res.y = y / rhs.y;
+        res.z = z / rhs.z;
+        return res;
+    }
+
+    float dotP(const vec3f &v) const {
+        return x * v.x + y * v.y + z * v.z;
+    }
 };
 
 struct vec3f8 { 
@@ -32,6 +65,34 @@ struct vec3f8 {
         res.y = add8(y, rhs.y);
         res.z = add8(z, rhs.z);
         return res;
+    }
+};
+
+struct sphere {
+    vec3f c;
+    float r;
+
+    sphere(vec3f c, float r) : c(c), r(r) {}
+
+    bool intersect(const vec3f &o, const vec3f &d) {
+        vec3f o_c = o - d;
+        float b = 2 * d.dotP(o_c);
+        float a = d.dotP(d);
+        float c = o_c.dotP(o_c) - (r * r);
+        float b24ac = (b * b) - 4 * a * c;
+
+	    if (b24ac < 0) {
+		    return false;
+	    }
+
+	    float t1 = (-b + std::sqrt(b24ac)) / 2.0f * a;
+	    float t2 = (-b - std::sqrt(b24ac)) / 2.0f * a;
+
+	    if (t1 < 0 || t2 < 0) {
+		    return false;
+	    } else {
+            return true;
+        }
     }
 };
 
@@ -50,5 +111,10 @@ void main() {
 
     vec3f8 res = a + b;
 
-    printf("%f, %f, %f", res.x.m256_f32[0], res.y.m256_f32[0], res.z.m256_f32[0]);
+    printf("%f, %f, %f\n", res.x.m256_f32[0], res.y.m256_f32[0], res.z.m256_f32[0]);
+
+    sphere s(vec3f(0, 0, 0), 5);
+    bool hit = s.intersect(vec3f(10, 0, 0), vec3f(-1, 0, 0));
+
+    printf("%d\n", hit);
 }
